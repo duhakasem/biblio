36f95edbb3db97fafc8133d4bd305302d0e961d8
Vanessa <160940850+vanessapest@users.noreply.github.com>
2025-05-01 19:04:14 +0200

    convert html to handlebars template

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 const express = require("express");
+const path = require('path');
 const server = express();
-const port = 3001;
+const port = 3000;
 
+// set handlebars as the view engine
+server.set('view engine', 'hbs');
+server.set('views', path.join(__dirname, 'views'));
+
+// middleware
+server.use(express.urlencoded({ extended: true }));
 server.use(express.static('public'));
 
-server.get("/", function (req, res) {
-    res.send(`
-        <h1>Welcome to the homepage!</h1>
-        <p><a href="/hello">Go to /hello</a></p>
-    `);
+// routes
+server.get("/review", function (req, res) {
+    res.render("personalrev");
 });
 
-server.get("/hello", function (req, res) {
-    res.send("Hello IMIs!");
-});
+server.post('/save-review', (req, res) => {
+    const { name, rating, review} = req.body;
+    res.render('review-thanks', { name, rating, review});
+})
 
-
+// start servers
 server.listen(port, function () {
     console.log("Express listening on " + port);
 });
