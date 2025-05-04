df38074dd3070026df820d9362ad6d729293099b
Santiago Badaya <125656966+sagobaday@users.noreply.github.com>
2025-05-04 16:06:23 +0200

    Implement book detail page with handlebars and Express route

--- index.js
+++ index.js
@@ -5,7 +5,19 @@ const books = JSON.parse(fs.readFileSync("./public/library.json", "utf-8"));
 const server = express();
 const port = 3001;
 
-const hbs = create({});
+const hbs = create({
+    // this takes care of the stars for the reviews
+    helpers: {
+        stars: (rating) => {
+          const fullStars = Math.round(rating);
+          let starsHtml = '';
+          for(let i = 0; i < 5; i++) {
+            starsHtml += i < fullStars ? '★' : '☆';
+          }
+          return starsHtml.split('');
+        }
+      }
+});
 
 server.engine("handlebars", hbs.engine);
 server.set("view engine", "handlebars");
@@ -13,8 +25,21 @@ server.set("views", "./views");
 
 server.use(express.static("public"));
 
+// Duha´s route for the landing page or feed
 server.get("/feed", function (req, res) {
     res.render("feed", { books });
+});
+
+// Santiago´s route for the book selection
+server.get('/books/:id', function(req, res) {
+    const bookId = parseInt(req.params.id);
+    const book = books.find(b => b.book_id === bookId);
+
+    if (book) {
+        res.render('book-detail', { book });
+    } else {
+        res.status(404).send('Book not found');
+    }
 });
 
 server.listen(port, function () {
